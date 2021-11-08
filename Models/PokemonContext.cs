b5dcb48d42f41ba8e78101bebbc28a20f00eb1a7
Sarwan Hakm <sarwan.h.najim@gmail.com>
2021-11-08 14:07:30 +0300

    Adds Pokemon controller and context classes

---
+++ Models/PokemonContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonWebApi_Auth0.Models
+{
+    public class PokemonContext
+    {
+        public List<Pokemon> pokemons;
+        public PokemonContext()
+        {
+            pokemons = new List<Pokemon>
+            {
+                new Pokemon
+                {
+                    Id=1,
+                    name="pokemon1",
+                    type="typ1"
+                },new Pokemon
+                {
+                    Id=1,
+                    name="pokemon2",
+                    type="typ2"
+                },new Pokemon
+                {
+                    Id=1,
+                    name="pokemon3",
+                    type="typ1"
+                }
+            };
+        }
+    }
+}
